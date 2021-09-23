c996ed6f8a830a43c92be60f71fd4d3bdb41bc1d
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2021-09-23 13:05:46 -0300

    Added collision to barrier, made it immovable

--- src/scripts/barrier/index.ts
+++ src/scripts/barrier/index.ts
@@ -1,22 +1,33 @@
 import GameScene from "../../scenes/Game";
 
 export class BarrierBlock extends Phaser.GameObjects.GameObject {
-    width : integer;
-    height : integer;
-    sprite : Phaser.Physics.Arcade.Sprite;
-    x : number;
-    y : number;
+  width: integer;
+  height: integer;
+  sprite: Phaser.Physics.Arcade.Sprite;
+  x: number;
+  y: number;
 
-    constructor(scene : GameScene, label : string, sprite : string, x : number, y : number) {
-        super(scene, label);
-        [this.x, this.y] = [x, y];
-        [this.width, this.height] = [80, 80];
-        this.sprite = scene.physics.add.sprite(x, y, label + "BarrierSprite");
-        this.sprite.setTexture(sprite);
-        this.sprite.setScale(this.width / this.sprite.width, this.height / this.sprite.height);
-    }
+  constructor(
+    scene: GameScene,
+    label: string,
+    sprite: string,
+    x: number,
+    y: number
+  ) {
+    super(scene, label);
+    [this.x, this.y] = [x, y];
+    [this.width, this.height] = [80, 80];
+    this.sprite = scene.physics.add.sprite(x, y, label + "BarrierSprite");
+    this.sprite.setTexture(sprite);
+    this.sprite.setScale(
+      this.width / this.sprite.width,
+      this.height / this.sprite.height
+    );
+    this.sprite.setImmovable();
+    this.scene.physics.add.collider(this.sprite, scene.player.sprite, () => {
+      console.debug("COLLIDED");
+    });
+  }
 
-    update() {
-
-    }
+  update() {}
 }
